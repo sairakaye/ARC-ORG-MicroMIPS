8b3de3becba90860111c46f96f5710b6cf0405b4
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-04-07 18:16:14 +0800

    GUI++

--- MicroMIPS/src/sample/Controller.java
+++ MicroMIPS/src/sample/Controller.java
@@ -1,6 +1,41 @@
 package sample;
 
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 
 public class Controller {
+    @FXML
+    private Button loadButton;
+    @FXML
+    private Button resetButton;
+    @FXML
+    private TextArea codingArea;
+    @FXML
+    private TableView opcodeTable;
+    @FXML
+    private TableColumn colInstruction;
+    @FXML
+    private TableColumn colHexOpcode;
+    @FXML
+    private TableColumn colBit31to26;
+    @FXML
+    private TableColumn colBit25to21;
+    @FXML
+    private TableColumn colBit20to16;
+    @FXML
+    private TableColumn colBit15to11;
+    @FXML
+    private TableColumn colBit10t06;
+    @FXML
+    private TableColumn colBit5to0;
+
+    private String[] savedCode;
+
+    @FXML
+    public void processCode() {
+        savedCode = codingArea.getText().split("\\n");
+    }
 }
