e2b0c92f2924e09bd7aba1bd67e03e3ee99f867a
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-04-12 03:50:03 +0800

    Added DADDIU and XORI checker methods.

--- MicroMIPS/src/sample/Controller.java
+++ MicroMIPS/src/sample/Controller.java
@@ -63,22 +63,24 @@ public class Controller implements Initializable{
 
     @FXML
     private void processCode() {
-        boolean error = false;
+        boolean isValid = false;
         savedCode = codingArea.getText().split("\\n");
 
-        /* This will be use for error handling in code
         for (String code: savedCode) {
-
+            if (code.contains("DADDIU"))
+                isValid = checkingDADDIU(code);
+            else if (code.contains("XORI"))
+                isValid = checkingXORI(code);
         }
 
-
+        /*
 
         if (!error) {
             makeInstructions();
         } else {
             // display here the error dialog
         }
-        */
+
         makeInstructions();
 
         ArrayList<OpcodeTableItem> opcodeTableItems = new ArrayList<>();
@@ -86,7 +88,7 @@ public class Controller implements Initializable{
 //        for (Instruction i: instructions) {
 //
 //        }
-
+*/
     }
 
     @FXML
@@ -138,5 +140,205 @@ public class Controller implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
         NPC = 0;
         registers = new HashMap<>();
+    }
+
+    public boolean checkingDADDIU(String codeLine) {
+        ArrayList<String> codeParts = new ArrayList<String>();
+        String[] splitter = codeLine.split(",");
+        StringBuilder errorMessage = new StringBuilder();
+
+        try {
+            if (splitter[0].contains(":")) {
+                // label
+                codeParts.add(splitter[0].substring(0, splitter[0].indexOf(":")));
+
+                // rt
+                String[] checker2 = splitter[0].substring(splitter[0].indexOf(":") + 1).trim().split("\\s+");
+
+                if (checker2[0].equalsIgnoreCase("DADDIU")) {
+                    codeParts.add(checker2[0]);
+
+                    if (checker2[1].startsWith("R")) {
+                        codeParts.add(checker2[1]);
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+
+            } else {
+                String[] checker2 = splitter[0].trim().split("\\s+");
+
+                if (checker2[0].equalsIgnoreCase("DADDIU")) {
+                    codeParts.add(checker2[0]);
+                    if (checker2[1].startsWith("R")) {
+                        codeParts.add(checker2[1]);
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            // for rs
+            if (splitter[1].trim().startsWith("R")) {
+                codeParts.add(splitter[1].trim());
+            } else {
+                return false;
+            }
+
+            String immediate = splitter[2].trim();
+
+            try {
+                if (immediate.startsWith("#")) {
+                    Integer toDecimal = Integer.parseInt(immediate.substring(immediate.indexOf("#") + 1), 16);
+
+                    if (!(toDecimal >= 0 && toDecimal <= 65535)) {
+                        return false;
+                    } else {
+                        codeParts.add(immediate.substring(immediate.indexOf("#")));
+                    }
+                } else if (immediate.startsWith("0x")) {
+                    int toDecimal = Integer.parseInt(immediate.substring(immediate.indexOf("x") + 1), 16);
+
+                    if (!(toDecimal >= 0 && toDecimal <= 65535)) {
+                        return false;
+                    } else {
+                        codeParts.add(immediate.substring(immediate.indexOf("0")));
+                    }
+                } else {
+                    int toDecimal = Integer.parseInt(immediate.substring(0));
+                    codeParts.add(Integer.toString(toDecimal));
+                }
+            } catch (Exception e) {
+                return false;
+            }
+
+            for (String c: codeParts)
+                System.out.println(c);
+
+            // Assuming all is correct
+            try {
+                int rt, rs;
+                if (!codeParts.get(0).equalsIgnoreCase("DADDIU")) {
+                    rt = Integer.parseInt(codeParts.get(2).substring(codeParts.get(2).indexOf("R") + 1));
+                    rs = Integer.parseInt(codeParts.get(3).substring(codeParts.get(3).indexOf("R") + 1));
+                } else {
+                    rt = Integer.parseInt(codeParts.get(1).substring(codeParts.get(1).indexOf("R") + 1));
+                    rs = Integer.parseInt(codeParts.get(2).substring(codeParts.get(2).indexOf("R") + 1));
+                }
+
+                if ((rt >= 0 && rt <= 31) && (rs >= 0 && rs <= 31))
+                    return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+
+        return false;
+    }
+
+    public boolean checkingXORI(String codeLine) {
+        ArrayList<String> codeParts = new ArrayList<String>();
+        String[] splitter = codeLine.split(",");
+        StringBuilder errorMessage = new StringBuilder();
+
+        try {
+            if (splitter[0].contains(":")) {
+                // label
+                codeParts.add(splitter[0].substring(0, splitter[0].indexOf(":")));
+
+                // rt
+                String[] checker2 = splitter[0].substring(splitter[0].indexOf(":") + 1).trim().split("\\s+");
+
+                if (checker2[0].equalsIgnoreCase("XORI")) {
+                    codeParts.add(checker2[0]);
+
+                    if (checker2[1].startsWith("R")) {
+                        codeParts.add(checker2[1]);
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+
+            } else {
+                String[] checker2 = splitter[0].trim().split("\\s+");
+
+                if (checker2[0].equalsIgnoreCase("XORI")) {
+                    codeParts.add(checker2[0]);
+                    if (checker2[1].startsWith("R")) {
+                        codeParts.add(checker2[1]);
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            // for r2
+            if (splitter[1].trim().startsWith("R")) {
+                codeParts.add(splitter[1].trim());
+            } else {
+                return false;
+            }
+
+            String immediate = splitter[2].trim();
+
+            try {
+                if (immediate.startsWith("#")) {
+                    Integer toDecimal = Integer.parseInt(immediate.substring(immediate.indexOf("#") + 1), 16);
+
+                    if (!(toDecimal >= 0 && toDecimal <= 65535)) {
+                        return false;
+                    } else {
+                        codeParts.add(immediate.substring(immediate.indexOf("#")));
+                    }
+                } else if (immediate.startsWith("0x")) {
+                    int toDecimal = Integer.parseInt(immediate.substring(immediate.indexOf("x") + 1), 16);
+
+                    if (!(toDecimal >= 0 && toDecimal <= 65535)) {
+                        return false;
+                    } else {
+                        codeParts.add(immediate.substring(immediate.indexOf("0")));
+                    }
+                } else {
+                    int toDecimal = Integer.parseInt(immediate.substring(0));
+                    codeParts.add(Integer.toString(toDecimal));
+                }
+            } catch (Exception e) {
+                return false;
+            }
+
+            for (String c: codeParts)
+                System.out.println(c);
+
+            // Assuming all is correct
+            try {
+                int rt = 0, rs = 0;
+                if (!codeParts.get(0).equalsIgnoreCase("XORI")) {
+                    rt = Integer.parseInt(codeParts.get(2).substring(codeParts.get(2).indexOf("R") + 1));
+                    rs = Integer.parseInt(codeParts.get(3).substring(codeParts.get(3).indexOf("R") + 1));
+                } else {
+                    rt = Integer.parseInt(codeParts.get(1).substring(codeParts.get(1).indexOf("R") + 1));
+                    rs = Integer.parseInt(codeParts.get(2).substring(codeParts.get(2).indexOf("R") + 1));
+                }
+
+                if ((rt >= 0 && rt <= 31) && (rs >= 0 && rs <= 31))
+                    return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+
+        return false;
     }
 }
